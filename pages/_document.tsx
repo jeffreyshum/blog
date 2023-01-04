1077ff38440aa1b42b13c598c9a93aac3e36a262
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2023-01-03 22:44:53 -0500

    Added Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,24 @@ const Document = () => {
 					integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
 					crossOrigin="anonymous"
 				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
 			</Head>
 			<body>
 				<Main />
