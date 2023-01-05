ca92a493d7f89d3a748eed8b2c49b4827387f4d3
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2023-01-04 20:44:04 -0500

    Added All Posts Page

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,13 +2,17 @@ import { FC } from "react"
 import Logo from "../Logo"
 import styles from "./Header.module.css"
 import ThemeButton from "../ThemeButton"
+import Link from "next/link"
 
 const Header: FC = () => {
 	return (
 		<header id={styles.header}>
-			<div id={styles.left}>
+			<nav id={styles.left}>
 				<Logo />
-			</div>
+				<Link href="/posts" className={styles.navLink}>
+					Posts
+				</Link>
+			</nav>
 			<div id={styles.right}>
 				<ThemeButton />
 			</div>
---
+++ pages/posts.tsx
@@ -0,0 +1,43 @@
+import { GetStaticProps, NextPage } from "next"
+import Head from "next/head"
+import { IndexProps } from "."
+import { fetchPosts } from "../utils"
+import styles from "../styles/index.module.css"
+import RecentPostCard from "../components/RecentPostCard"
+
+const Posts: NextPage<IndexProps> = ({ recentPosts }) => {
+	return (
+		<>
+			<Head>
+				<title>Jeffrey Shum - Blog</title>
+				<meta
+					name="description"
+					content="All posts from Jeffrey Shum's Blog"
+				/>
+				<meta name="keywords" content="Jeffrey Shum, Blog" />
+				<meta name="author" content="Jeffrey Shum" />
+				<meta name="og:title" content="All Posts" />
+				<meta
+					name="og:description"
+					content="All posts from Jeffrey Shum's Blog"
+				/>
+			</Head>
+			<div className={styles.title}>
+				<h1>All Posts</h1>
+			</div>
+			{recentPosts.map((post, i) => (
+				<RecentPostCard {...post} key={i} />
+			))}
+		</>
+	)
+}
+
+export default Posts
+
+export const getStaticProps: GetStaticProps = async () => {
+	return {
+		props: {
+			recentPosts: fetchPosts(),
+		},
+	}
+}
