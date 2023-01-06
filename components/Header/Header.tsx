137622d9ca732f1cac2ac2d1ff0cfc5623ae394e
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2023-01-05 21:13:17 -0500

    Added Hamburger Icon Without Mobile Nav

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,14 +7,22 @@ import Link from "next/link"
 const Header: FC = () => {
 	return (
 		<header id={styles.header}>
-			<nav id={styles.left}>
+			<div id={styles.left}>
 				<Logo />
+			</div>
+			<nav id={styles.right}>
+				<ThemeButton />
 				<Link href="/posts" className={styles.navLink}>
 					Posts
 				</Link>
 			</nav>
-			<div id={styles.right}>
-				<ThemeButton />
+			<div id={styles.mobile}>
+				<input className={styles.toggle} type="checkbox" />
+				<div id={styles.hamburger}>
+					<span className={styles.line} id={styles.line1} />
+					<span className={styles.line} id={styles.line2} />
+					<span className={styles.line} id={styles.line3} />
+				</div>
 			</div>
 		</header>
 	)
