706ec3fcf6252c63a9b4de42c8afa38f38fc1043
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2023-01-13 18:03:20 -0500

    Added Custom Label Capability for TabbedView

--- components/ArticleComponents/TabbedView/TabbedView.tsx
+++ components/ArticleComponents/TabbedView/TabbedView.tsx
@@ -1,10 +1,37 @@
 import { Children, FC, PropsWithChildren, useState } from "react"
 import styles from "./TabbedView.module.css"
 
-const TabbedView: FC<PropsWithChildren> = (props) => {
+interface TabbedViewProps extends PropsWithChildren {
+	labels?: boolean
+}
+
+const TabbedView: FC<TabbedViewProps> = (props) => {
 	const childArray = Children.toArray(props.children)
 
-	const [activeTab, setActiveTab] = useState(0)
+	const [activeTab, setActiveTab] = useState(props.labels ? 1 : 0)
+
+	if (props.labels) {
+		return (
+			<div>
+				<div className={styles.selectContainer}>
+					{childArray
+						.filter((child, index) => index % 2 === 0)
+						.map((Label, index) => (
+							<div
+								key={index}
+								onClick={(e) => {
+									setActiveTab(index * 2 + 1)
+								}}
+								className={styles.selectItem}
+							>
+								{Label}
+							</div>
+						))}
+				</div>
+				<div>{childArray[activeTab]}</div>
+			</div>
+		)
+	}
 
 	return (
 		<div>
