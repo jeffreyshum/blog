8047e4f75f4961c8767400c09635c9becba762fb
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2023-02-07 13:05:57 -0500

    Fixed Hydration Mismatch Issue

--- components/RecentPostCard/RecentPostCard.tsx
+++ components/RecentPostCard/RecentPostCard.tsx
@@ -2,23 +2,30 @@ import Link from "next/link"
 import { FC } from "react"
 import styles from "./RecentPostCard.module.css"
 import { Post } from "../../pages"
+import { useRouter } from "next/router"
 
 const RecentPostCard: FC<Post> = (props) => {
+	const router = useRouter()
+
 	return (
-		<div className={styles.card}>
-			<Link href={props.url}>
-				<h1 className={styles.title}>{props.title}</h1>
-				<p className={styles.description}>{props.description}</p>
-				<div className={styles.linkContainer}>
-					<p className={styles.button}>Read More</p>
-					<Link
-						href={`/posts/${props.category}`}
-						className={styles.category}
-					>
-						{props.category}
-					</Link>
-				</div>
-			</Link>
+		<div
+			className={styles.card}
+			onClick={(e) => {
+				e.preventDefault()
+				router.push(props.url)
+			}}
+		>
+			<h1 className={styles.title}>{props.title}</h1>
+			<p className={styles.description}>{props.description}</p>
+			<div className={styles.linkContainer}>
+				<p className={styles.button}>Read More</p>
+				<Link
+					href={`/posts/${props.category}`}
+					className={styles.category}
+				>
+					{props.category}
+				</Link>
+			</div>
 		</div>
 	)
 }
