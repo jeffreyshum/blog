2b423d30aca9970b41d7d629b1cb72f8e21ee9a1
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2023-02-12 22:59:04 -0500

    Added styled-components Configuration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,41 +1,74 @@
-import { Head, Html, Main, NextScript } from "next/document"
+import Document, {
+	DocumentContext,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
-const Document = () => {
-	return (
-		<Html lang="en">
-			<Head>
-				<meta charSet="utf-8" />
-				<link
-					rel="stylesheet"
-					href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
-					integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
-					crossOrigin="anonymous"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	)
+export default class AppDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const sheet = new ServerStyleSheet()
+		const originalRenderPage = ctx.renderPage
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
+				})
+
+			const initialProps = await Document.getInitialProps(ctx)
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			}
+		} finally {
+			sheet.seal()
+		}
+	}
+
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<meta charSet="utf-8" />
+					<link
+						rel="stylesheet"
+						href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
+						integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
+						crossOrigin="anonymous"
+					/>
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
 }
-
-export default Document
