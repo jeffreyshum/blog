b55a16ba5439a6671b7337aa83ff06996e48c990
jeffreyshum <89279966+jeffreyshum@users.noreply.github.com>
2023-02-12 23:47:34 -0500

    Moved CSS to styled-components

--- components/RecentPostCard/RecentPostCard.tsx
+++ components/RecentPostCard/RecentPostCard.tsx
@@ -1,32 +1,73 @@
 import Link from "next/link"
 import { FC } from "react"
-import styles from "./RecentPostCard.module.css"
+import styled, { css } from "styled-components"
 import { Post } from "../../pages"
 import { useRouter } from "next/router"
 
+const Underline = css`
+	border-bottom: 1px solid transparent;
+	transition: border-color var(--theme-change-transition);
+	width: fit-content;
+
+	&:hover {
+		border-color: var(--text-color-article-link);
+	}
+`
+const Card = styled.div`
+	border: 1px solid transparent;
+	padding: 0.5rem;
+	border-radius: 5px;
+	margin-bottom: 1rem;
+	cursor: pointer;
+
+	&:hover {
+		border-color: var(--text-color-article-link);
+	}
+`
+
+const CardTitle = styled.h1`
+	font-size: 1.5rem;
+	${Underline}
+`
+
+const CardDescription = styled.p``
+
+const LinkContainer = styled.div`
+	display: flex;
+`
+
+const ReadMore = styled.span`
+	width: fit-content;
+	${Underline}
+`
+
+const Category = styled.span`
+	margin-left: auto;
+	color: var(--font-color-category);
+`
 const RecentPostCard: FC<Post> = (props) => {
 	const router = useRouter()
 
 	return (
-		<div
-			className={styles.card}
+		<Card
 			onClick={(e) => {
 				e.preventDefault()
 				router.push(props.url)
 			}}
 		>
-			<h1 className={styles.title}>{props.title}</h1>
-			<p className={styles.description}>{props.description}</p>
-			<div className={styles.linkContainer}>
-				<p className={styles.button}>Read More</p>
-				<Link
-					href={`/posts/${props.category}`}
-					className={styles.category}
-				>
-					{props.category}
-				</Link>
-			</div>
-		</div>
+			<CardTitle>{props.title}</CardTitle>
+			<CardDescription>{props.description}</CardDescription>
+			<LinkContainer>
+				<ReadMore>
+					<Link href={props.url}>Read More</Link>
+				</ReadMore>
+				<Category>
+					<Link href={`/posts/${props.category}`}>
+						{props.category}
+					</Link>
+				</Category>
+			</LinkContainer>
+		</Card>
 	)
 }
 
